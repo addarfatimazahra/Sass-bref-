24df9acc92db6c88275a933c09ba299f8de9b9e9
addarfatimazahra <135432903+addarfatimazahra@users.noreply.github.com>
2024-11-28 12:05:46 +0000

    change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// Prix des produits
+
 const prixProduits = {
-    produit1: 400, // Prix du fond de teint
-    produit2: 300, // Prix du mascara
-    produit3: 400  // Prix du rouge à lèvres
+    produit1: 400, 
+    produit2: 300, 
+    produit3: 400  
 };
 
-// Fonction pour calculer le prix total
+
 function calculerPrixTotal() {
     const quantiteInput = document.getElementById('Quantite').value;
     const produitSelectionne = document.querySelector('input[name="produit"]:checked');
@@ -18,11 +18,11 @@ function calculerPrixTotal() {
     const prixUnitaire = prixProduits[produitSelectionne.value];
     const total = prixUnitaire * quantiteInput;
 
-    // Mettre à jour le prix total affiché
+    
     document.getElementById('total-price').textContent = `Prix Totale : ${total} DH`;
 }
 
-// Ajouter un écouteur d'événements pour le formulaire
+
 document.getElementById('form-panier').addEventListener('submit', function(event) {
     event.preventDefault();
     calculerPrixTotal();
